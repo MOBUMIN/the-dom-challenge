6bc79a3a436acc52c7fd03ffabf4e009bfc27bd8
김수빈 <57705512+MOBUMIN@users.noreply.github.com>
2022-07-27 21:21:48 +0900

    challenge 4 완성

--- color-spotter/script.js
+++ color-spotter/script.js
@@ -1,17 +1,16 @@
 const INIT_ROWS = 4;
-const INIT_COLS = 4;
+const ROWS_RANGE = 10;
 
 let answer = null;
 let score = null;
+let wrong = false;
 
 function GameBoard(rows) {
 	const grid = document.querySelector('#grid');
     
-    // initDom(grid);
-    
+    initDom(grid);
 	makeDOM(grid, rows, rows);
-	setAnswerEl(rows * rows);
-    attachClickEvent(grid);
+    if(score === null) attachClickEvent(grid);
 }
 
 function Score(num) {
@@ -36,40 +35,53 @@ const getRandomColors = () => {
     }
 }
 
-const makeDom = (grid, rows, cols) => {
+const makeDOM = (grid, rows, cols) => {
+	const colorSet = getRandomColors();
+	answer = getRandomNum(rows * cols);
     for(let i=0; i<rows; i++) {
         const row = document.createElement('div');
         row.className = 'row';
         grid.appendChild(row);
         for (let j=0; j<cols; j++) {
+			const index = rows * i + j;
+
             const square = document.createElement('div');
-            square.dataset.index= rows*i + cols*j;
+			square.className = 'square'
+			square.style.backgroundColor = index === answer ? colorSet.oddColor : colorSet.color;
+            square.dataset.index= index;
             row.appendChild(square);
         }
     }
+	if(wrong) {
+		grid.classList.add('shake');
+		setTimeout(() => {
+			grid.classList.remove('shake');
+		}, 800)
+		wrong = false;
+	}
 }
 
-const getRandomNum = (num) => Math.floor(Math.random() * num);
-
-const setAnswerEl = (num) => {
-   answer = getRandomNum(num);
-}
+const getRandomNum = (num) => Math.floor(Math.random() * num) + 2;
 
 const compareAnswer = (compNum) => compNum === answer;
 
 const attachClickEvent = (grid) => {
     grid.addEventListener('click', (e) => {
-        const result = compareAnswer(e.target.dataset.index);
+        const result = compareAnswer(Number(e.target.dataset.index));
         if(result) {
             Score(score+1);
-            GameBoard(getRandomNum(100))
+            GameBoard(getRandomNum(ROWS_RANGE))
         }
+		else {
+			wrong = true;
+			Score(0);
+			GameBoard(INIT_ROWS)
+		}
     });
 }
 
 const initDom = (grid) => {
     while(grid.hasChildNodes()) {
-        console.log(grid);
-        grid.removeChild();
+        grid.removeChild(grid.firstChild);
     }
 }
